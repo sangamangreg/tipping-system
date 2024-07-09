aacc8c13d62d1ab14dfe9fb73d622655dd896912
Sangam Angre <sangam.angre@gmail.com>
2024-07-09 11:56:43 +0530

    Added required controllers, models, helpers and their rspec files.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+
+end
---
+++ spec/helpers/products_helper_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ProductsHelper, type: :helper do
+
+end
