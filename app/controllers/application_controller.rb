896cab02df45b5a85862a215dadea71830e7dc8c
Sangam Angre <sangam.angre@gmail.com>
2024-07-09 12:54:28 +0530

    Product details page with reviews added. Also created a small tipping system to the platform.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def not_found
+    Rails.logger.info("404 page shown")
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
+  def clear_session
+    session.delete(:product)
+    session.delete(:percent)
+    session.delete(:cart_amount)
+  end
 end
