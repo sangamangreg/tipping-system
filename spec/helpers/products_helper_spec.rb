5457f8f6eaee01e9802c0b63c2e4f6c61e1ab56d
Sangam Angre <sangam.angre@gmail.com>
2024-07-09 13:26:02 +0530

    Rspecs added for required files

--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -3,5 +3,17 @@
 require "rails_helper"
 
 RSpec.describe ProductsHelper, type: :helper do
+  describe "validate currency_symbol" do
+    it "should return usd symbol default" do
+      expect(ProductsHelper.currency_symbol).to eq("$")
+    end
 
+    it "should return inr symbol default" do
+      expect(ProductsHelper.currency_symbol("INR")).to eq("₹")
+    end
+
+    it "should return china symbol default" do
+      expect(ProductsHelper.currency_symbol("CHN")).to eq("$")
+    end
+  end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -3,5 +3,33 @@
 require "rails_helper"
 
 RSpec.describe ProductsController, type: :controller do
+  describe "GET /" do
+    it 'should render success' do
+      expect(Rails.logger).to receive(:info).exactly(2).times
+      controller.send(:home)
+    end
+  end
 
+  describe "GET /show" do
+    it 'should render success' do
+      expect(Product).to receive(:get).and_return(Product.new)
+      expect(Rails.logger).to receive(:info).exactly(1).times
+      expect(response).to have_http_status(:success)
+      controller.send(:show)
+    end
+  end
+
+  describe "GET /checkout" do
+    # it 'should render success' do
+    #   product = Product.new
+    #   product.price = 10
+    #   expect(Rails.logger).to receive(:info).exactly(1).times
+    #   expect(Product).to receive(:get).and_return(product)
+    #   session[:percent] = 10
+    #   expect(response).to render_template "checkout"
+    #   controller.send(:checkout)
+    #   expect(session[:cart_amount]).to eq(11)
+    #   session.delete(:percent)
+    # end
+  end
 end
